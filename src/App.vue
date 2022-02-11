afdbaec8073aee46a81f3645f50ddd367c030740
Glitch (tapestory-shapes-2) <none>
2022-02-11 21:13:59 +0000

    TS Constants

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="ph2">
     <main class="sans-serif w-99 measure center mv4">
-      <box :boxModel="boxModel" />
+      <box :box-model="boxModel" />
 
       <!-- Palette Selector -->
-      <div class="mv4 ba br4 overflow-hidden">
+      <div class="mv4 neu overflow-hidden">
         <div class="overflow-x-auto pv3 ph2 ws-nw">
           <div
             v-for="p in palettes"
@@ -23,7 +23,7 @@
       </div>
 
       <!-- Shader Selector -->
-      <div class="mv4 ba br4 overflow-hidden">
+      <div class="mv4 neu overflow-hidden">
         <div class="overflow-x-auto pv3 ph2 ws-nw">
           <div
             v-for="s in shaders"
@@ -36,7 +36,7 @@
       </div>
 
       <!-- Forground Selector -->
-      <div class="mv4 ba br4 overflow-hidden">
+      <div class="mv4 neu overfneulow-hidden">
         <div class="overflow-x-auto pv3 ph2 ws-nw">
           <div
             v-for="f in foregrounds"
@@ -68,23 +68,7 @@
 import box from "./components/Box.vue";
 import { reactive } from "vue";
 import { type IBox } from "@/types/IBox";
-
-// Options
-// TODO Import from TS files?
-const palettes = [
-  0, 1, 3, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 23, 26, 27, 28,
-  29, 34, 35, 37, 38, 40, 45, 47, 50, 55, 57, 60, 67, 69, 71, 73,
-];
-const shaders = ["1", "2", "3", "4", "5", "g13", "g35", "g135", "a", "b"];
-const foregrounds = [
-  "black",
-  "white",
-  "first",
-  "second",
-  "third",
-  "fourth",
-  "fifth",
-];
+import { palettes, shaders, foregrounds } from "@/data/constants.ts";
 
 // Selection
 const boxModel = reactive<IBox>({
---
+++ src/data/constants.ts
@@ -0,0 +1,16 @@
+export const palettes : number[] = [
+  0, 1, 3, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 18, 20, 22, 23, 26, 27, 28,
+  29, 34, 35, 37, 38, 40, 45, 47, 50, 55, 57, 60, 67, 69, 71, 73,
+];
+
+export const shaders : string[] = ["1", "2", "3", "4", "5", "g13", "g234", "g35", "g135", "c1", "c2", "h23", "h34"];
+
+export const foregrounds : string[] = [
+  "black",
+  "white",
+  "first",
+  "second",
+  "third",
+  "fourth",
+  "fifth",
+];
